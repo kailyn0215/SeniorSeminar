69dc4a80059688b7c9893619b4806b29b904123a
kailyn0215 <brownkai24@7hills.org>
2024-01-16 12:47:19 -0500

    renamed excel sheets, added session file, tried to fix tester but it wont print from file :(

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
 public class Student {
-    
+    public Student(int time, String name, String email, int choice1, int choice2, int choice3, int choice4, int choice5) {
+
+    }
 }
--- Tester.java
+++ Tester.java
@@ -2,20 +2,31 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.*;
 
+/*i have no idea how to parse through a excel sheet so im putting these here are reference for future when i implement it 
+  https://stackoverflow.com/questions/1516144/how-to-read-and-write-excel-file
+  https://poi.apache.org/
+  i dont think we learned this in class so idk what else to do :(
+*/
+
 public class Tester {
     public static void main(String[] args) {
         ArrayList<Student> studentList = new ArrayList<Student>();
+        ArrayList<String> sessions = new ArrayList<String>();
+
         try {
-            File students = new File("Senior Seminar Sessions.xlsx");
+            File students = new File("SeniorSeminarSessions.xlsx");
             Scanner myReader = new Scanner(students);
+            int count = 0;
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                String currentSession= myReader.nextLine();
+                sessions.add(currentSession);
+                System.out.println("Added " + sessions.get(count) + " to session list");
+                count++;
             }
             myReader.close();
         } 
         catch (FileNotFoundException e) {
-            System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'Senior Seminar Sessions.xlsx' and 'Senior Seminar Student.xlsx' into the program.");
+            System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'SeniorSeminarSessions.xlsx' into the program.");
             e.printStackTrace();
         }
     }
