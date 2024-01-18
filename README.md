50c31d26e202c0a3e0ee14853cec418e9789dfb5
Kailyn Brown <brownkai24@7hills.org>
2024-01-18 13:06:40 -0500

    updated read me + started try catch for students file upload

--- README.md
+++ README.md
@@ -17,3 +17,18 @@ Plan:
 - (possibly) go down the list of their first choices and assign based off of that then continue for 2nd 3rd etc... - not sure abt this yet bc im not sure how it will turn out
 - while going down the list keep track of which ones are most popular/least popular (points system 5 pts for first choice, 1 pt for last, etc) + which ones need a second session (at least 7 i think)
 - maybe array thing of rooms & times?
+
+1/18 update:
+figured out how to read the excel files (i know its dumb)
+got the try catch for sessions working + printing the session names, ids, and proctors
+started the try catch for students
+added to the session.java file to get the sessions to work (added variables etc)
+added some variables to tester as well in order to get try catch working
+also thought about how to optimize the students choices, maybe go to the side based on popularity? ex:
+time: 1, 2, 3, 4, 5
+rooms:
+1 --- 1, 2, 3, 4, 5
+2 --- 6, 7, 8, 9, 10
+
+maybe more of a change for people to get their top choices since the popular ones wont all be in timeslot 1?
+
--- Tester.java
+++ Tester.java
@@ -5,30 +5,38 @@ import java.util.*;
 public class Tester {
     public static void main(String[] args) {
         ArrayList<Student> studentList = new ArrayList<Student>();
-        ArrayList<Session> sessions = new ArrayList<Session>();
+        ArrayList<Session> sessionList = new ArrayList<Session>();
         
         String tempName; //temporary name holder for sessions
         int tempId; //temporary id for sessions
         String tempProctor; //temporary proctor name for sessions
 
-        try {
-            File students = new File("SeniorSeminarSessions.xlsx - Sheet1.csv");
-            Scanner myReader = new Scanner(students);
-            int count = 0;
-            while (myReader.hasNextLine()) {
-                String currentSession[] = myReader.nextLine().split(",");
+        try { //try catch for sessions data table
+            File sessions = new File("SeniorSeminarSessions.xlsx - Sheet1.csv");
+            Scanner seshScanner = new Scanner(sessions);
+            int count = 0; //i dont remember why i added this rn </3
+            while (seshScanner.hasNextLine()) {
+                String currentSession[] = seshScanner.nextLine().split(",");
                 tempName = currentSession[0];
                 tempId = Integer.parseInt(currentSession[1]);
                 tempProctor = currentSession[2];
-                sessions.add(new Session(tempName, tempId, tempProctor));
+                sessionList.add(new Session(tempName, tempId, tempProctor));
                 count++;
             }
             System.out.println("Finished session try catch for a total of " + (count) + " sessions.");
-            myReader.close();
+            seshScanner.close();
         } 
         catch (FileNotFoundException e) {
             System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'SeniorSeminarSessions.xlsx - Sheet1.csv' into the program.");
             e.printStackTrace();
         }
+
+        /*try {
+            File students = new File("SeniorSeminarStudent.xlsx - Sheet1.csv");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'SeniorSeminarSessions.xlsx - Sheet1.csv' into the program.");
+            e.printStackTrace();
+        }*/
     }
 }
