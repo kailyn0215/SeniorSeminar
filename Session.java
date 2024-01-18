f9ec78eabf4463cab5196780dbf8defec73136eb
Kailyn Brown <brownkai24@7hills.org>
2024-01-18 12:53:14 -0500

    finished try catch for sessions !!

--- Session.java
+++ Session.java
@@ -1,3 +1,13 @@
 public class Session {
-    
+
+    private String name;
+    private int id;
+    private String proctor;
+
+    public Session(String seshName, int seshId, String seshProctor) {
+        name = seshName;
+        id = seshId;
+        proctor = seshProctor;
+        System.out.println("Added " + name + ", ID: " + id + ", Proctored by " + proctor);
+    }    
 }
--- Tester.java
+++ Tester.java
@@ -2,31 +2,32 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.*;
 
-/*i have no idea how to parse through a excel sheet so im putting these here are reference for future when i implement it 
-  https://stackoverflow.com/questions/1516144/how-to-read-and-write-excel-file
-  https://poi.apache.org/
-  i dont think we learned this in class so idk what else to do :(
-*/
-
 public class Tester {
     public static void main(String[] args) {
         ArrayList<Student> studentList = new ArrayList<Student>();
-        ArrayList<String> sessions = new ArrayList<String>();
+        ArrayList<Session> sessions = new ArrayList<Session>();
+        
+        String tempName; //temporary name holder for sessions
+        int tempId; //temporary id for sessions
+        String tempProctor; //temporary proctor name for sessions
 
         try {
-            File students = new File("SeniorSeminarSessions.xlsx");
+            File students = new File("SeniorSeminarSessions.xlsx - Sheet1.csv");
             Scanner myReader = new Scanner(students);
             int count = 0;
             while (myReader.hasNextLine()) {
-                String currentSession= myReader.nextLine();
-                sessions.add(currentSession);
-                System.out.println("Added " + sessions.get(count) + " to session list");
+                String currentSession[] = myReader.nextLine().split(",");
+                tempName = currentSession[0];
+                tempId = Integer.parseInt(currentSession[1]);
+                tempProctor = currentSession[2];
+                sessions.add(new Session(tempName, tempId, tempProctor));
                 count++;
             }
+            System.out.println("Finished session try catch for a total of " + (count) + " sessions.");
             myReader.close();
         } 
         catch (FileNotFoundException e) {
-            System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'SeniorSeminarSessions.xlsx' into the program.");
+            System.out.println("Uh oh! It looks like one of the files hasn't been loaded in! Please load 'SeniorSeminarSessions.xlsx - Sheet1.csv' into the program.");
             e.printStackTrace();
         }
     }
